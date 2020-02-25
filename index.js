21634345770138b8c35813d9a6de90a152d33291
Dustin Myers <dustinblaine.myers@gmail.com>
2020-02-25 10:51:19 -0500

    Feat: add teacher and convict

--- index.js
+++ index.js
@@ -28,6 +28,44 @@ class Person {
 Create an object called Teacher derived from the Person class, and implement a method called teach which receives a string called subject, and prints out:
 */
 
+//below I got some practice with the Object.create method.
+// const Teacher = Object.create(Person, {
+
+//     name: {value: "Betty"},
+//     age: {value: 45},
+//     teach: {
+
+//         value: function(subject) {
+
+//             console.log(`Welcome to ${subject}, my name is ${this.name}`);
+
+//         }
+//     } 
+  
+// })
+
+
+
+class Teacher extends Person {
+
+    constructor(name, age, subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+    
+    teach() {
+        console.log(`Welcome to ${this.subject}, my name is ${this.name}`);
+
+    }
+
+}
+
+class Convict extends Person {
+
+    proclaimInnocence(crime) {
+        console.log(`I am not guilty of ${crime} , I swear`);
+    }
+}
 const me = new Person("Dustin",32);
 
 console.log(me.describe());
@@ -35,3 +73,12 @@ console.log(me.describe());
 const Larry = new Person("Larry",)
 
 me.sayHello();
+
+
+const Betty = new Teacher("Betty", 32, "math");
+
+console.log(Betty.teach());
+
+const Walter = new Convict("Walter", 23);
+
+Walter.proclaimInnocence('shoplifting');
